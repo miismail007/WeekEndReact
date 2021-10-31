05a3650bf2e0eef71c02072a9ace0d47432e6d93
ismail-anywhere <mohammed.abdul@anywhere.co>
2021-10-31 15:13:13 +0530

    added counter state

--- src/App.js
+++ src/App.js
@@ -7,13 +7,20 @@ function App() {
   const [logo , setLogo ] = useState("Fita")
   const [menu , setMenu ] = useState(['Home' , 'About' , 'Contact' , 'Gallery'])
   const [products , setProducts] = useState([])
+  const [counter , setCounter] = useState(0)
 
   const setProducttoState = (obj) => {
     setProducts([...products,obj])
   }
   return (
     <div>
-      
+      <button onClick={()=>{
+        setCounter(counter+1)
+      }}>increment</button>
+      <button onClick={()=>{
+        setCounter(counter-1)
+      }}>decrement</button>
+      <h1>{Counter}</h1>
       <Header logo = {logo} menu = {menu}/>
       <Form setProducttoState={setProducttoState}/>
       <Products products = {products}/>
